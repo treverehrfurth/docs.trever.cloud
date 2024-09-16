37d17f4861b388304ad31f2232f510339505d484
Trever Ehrfurth <trever.ehrfurth@gmail.com>
2024-09-16 17:02:11 -0500

    ooops

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1726521988',resources: [
+    cacheName: 'chirpy-1726523625',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
